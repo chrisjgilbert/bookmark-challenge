7c5013494967a13e4b735abc7b8b730e3397d195
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-19 16:59:31 +0000

    .all test updated

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,9 +2,14 @@ require 'bookmark'
 
 describe Bookmark do
 
-  describe '#all' do
-    it 'returns an array of book marks' do
-      expect(Bookmark.all).to be_a(Array)
+  describe '.all' do
+    it 'returns a list of bookmarks' do
+      bookmarks = Bookmark.all
+
+      expect(bookmarks).to include "http://makers.tech"
+      expect(bookmarks).to include "http://www.destroyallsoftware.com"
+      expect(bookmarks).to include "http://www.google.com"
     end
   end
+
 end
