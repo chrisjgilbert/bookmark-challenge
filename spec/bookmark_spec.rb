0960333943867baa3b0c7c9b742d5e5e4bc4b88d
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-19 17:14:25 +0000

    database connected

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -6,7 +6,7 @@ describe Bookmark do
     it 'returns a list of bookmarks' do
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include "http://makers.tech"
+      expect(bookmarks).to include "http://www.makersacademy.com"
       expect(bookmarks).to include "http://www.destroyallsoftware.com"
       expect(bookmarks).to include "http://www.google.com"
     end
