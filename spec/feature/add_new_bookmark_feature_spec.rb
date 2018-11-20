f17db228052f61289c56f5d919a275a79eb59c79
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-20 19:02:30 +0000

    refactored

--- spec/feature/add_new_bookmark_feature_spec.rb
+++ spec/feature/add_new_bookmark_feature_spec.rb
@@ -2,10 +2,10 @@ require 'pg'
 
 feature 'adding bookmarks' do
   scenario 'add a new bookmark' do
-    visit '/bookmarks/new'
+    visit '/bookmarks'
+    click_button 'Add Bookmarks'
     fill_in :url, with: "http://www.hello.com"
-    click_button 'Add Bookmark'
-
+    click_button 'Add New Bookmark'
     expect(page).to have_content "http://www.hello.com"
   end
 end
